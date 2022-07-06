22b0339e6b3b4e44065fd65d66a8e3d48dc4a928
Charlie Batchelor <charliebatchelor1@hotmail.co.uk>
2022-07-06 18:52:12 +0200

    More cleanup, open web page template

--- countUp.py
+++ countUp.py
@@ -4,15 +4,18 @@ import pandas as pd
 from datetime import datetime
 
 # My files to use when updating the google dataframe
-file_list = ['charlieActivities.csv', 'matthewActivities.csv', 'georgeActivities.csv', 'rhysActivities.csv', "finchActivities.csv"]
-name = ["Charlie", "Matthew", "George", "Rhys", "Finch"]
+file_list = ['charlieActivities.csv', 'matthewActivities.csv', 'georgeActivities.csv',
+             'rhysActivities.csv', "finchActivities.csv"]
+
+names = ["Charlie", "Matthew", "George", "Rhys", "Finch"]
 
 # Read google sheet data to a data frame
 googleData = pd.read_csv("googleData.csv")
 # Date Variable - eg 2022-02-01
 now = datetime.now()
 stringDate = now.strftime("%Y-%m")
-# stringDate = '2022-04' #artifici  al string to pull back month 2022-xx
+print("String date: ", stringDate)
+# stringDate = '2022-06' #artifici  al string to pull back month 2022-xx
 run = "Run" # Run string to compare type later
 group_pace = 0
 group_pace_sum = 0
@@ -20,6 +23,8 @@ group_all_runs = 0
 
 # loop over all input files and get data to right people
 for i, input_file in enumerate(file_list):
+
+    # get useful info and initialise variables for new person
     input_file = "./activities/" + str(input_file)
     data = pd.read_csv(input_file, sep=",", header=0)
     dis = data["distance"]
@@ -28,7 +33,7 @@ for i, input_file in enumerate(file_list):
     elev = data["total_elevation_gain"]
     mov_t = data["moving_time"]
     no_acts = len(dis)  # Number of activities
-    tot_distance = 0 # total distance for month
+    tot_distance = 0  # total distance for month
     tot_el = 0
     avg_pace_sum = 0
     all_runs = 0 # all runs, below 2km too for pace normalisation
@@ -40,7 +45,7 @@ for i, input_file in enumerate(file_list):
     # Tally up the stuff we want to send to google doc
     for j in range(no_acts):
         start_date = date[j][0:7]
-        if type[j] == run and start_date == stringDate:
+        if type[j] == run and start_date == stringDate and dis[j] != 0 and mov_t[j] != 0:
             tot_distance += dis[j]
             tot_el += elev[j]
             all_runs += 1
@@ -63,11 +68,11 @@ for i, input_file in enumerate(file_list):
             pace = str(int(mins)) + ":" + str(int(secs))
             avg_el = tot_el/all_runs
 
-        if(all_runs == 0):
+        if all_runs == 0:
             pace = str(0)
 
     for k, n in enumerate(googleData["Name"]):
-        if n == name[i]:
+        if n == names[i]:
             googleData.loc[k, "Distance"] = tot_distance/1000
             googleData.loc[k, "NumRuns2k"] = total_2k
             googleData.loc[k, "NumRuns5k"] = total_5k
@@ -76,11 +81,6 @@ for i, input_file in enumerate(file_list):
             googleData.loc[k, "AveragePace"] = pace
             googleData.loc[k, "AvgElPerRun"] = avg_el
 
-    # Reset variables for next person
-    tot_distance = 0
-    num_runs = 0
-
-# Reprint updated data
 # print(googleData.to_string())
 googleData.to_csv("googleData.csv", index=False)
 
--- website/server.js
+++ website/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const { dirname } = require("path");
 const path = require("path");
 
 const app = express();
 
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080, () => console.log("Server running..."));
