d0fbb5a46eb8e085434e1f841ee1d76c3c081967
Charlie Batchelor <charliebatchelor1@hotmail.co.uk>
2023-02-12 22:53:40 +0000

    Updating the new plotter.

--- analysisGraphs/monthlyDistancesLinePlot.py
+++ analysisGraphs/monthlyDistancesLinePlot.py
@@ -1,6 +1,7 @@
 # =====================================================
-# Code to plot 3 subplots, showing the average pace for
-# each run of each person.
+# Plots the last X months total distances ran, for each
+# Nug of the NugRunClub.
+# =====================================================
 import pandas as pd
 from matplotlib import pyplot as plt
 import sys
@@ -10,13 +11,12 @@ from dateutil.relativedelta import *
 
 # Variables
 run = "Run"
-numMonths = 12  # Number of months to go back
-
+numMonths = 24  # Number of months to go back
 names = ["Charlie", "Matthew", "George", "Rhys", "Finch"]
 file_list = ['../activities/charlieActivities.csv', '../activities/matthewActivities.csv', '../activities/georgeActivities.csv',
              '../activities/rhysActivities.csv', "../activities/finchActivities.csv"]
 
-# Make a list of months that I can iterate through
+# Make a list of months that we can iterate through
 now = datetime(datetime.now().year, datetime.now().month, 1)
 then = (now + relativedelta(months=-numMonths))
 months_list = []
@@ -24,12 +24,14 @@ while then <= now:
     then += timedelta(days=32) # This should shift us to the next month
     months_list.append(datetime(then.year, then.month, 1).strftime('%Y-%m'))
 
-# Set up figure
+# Set up figure space
 fig = plt.subplot(111)
-fig.set_title("Monthly Total Distances - Past 12 Months", fontweight="bold")
+title = "Monthly Total Distances - Past " + str(numMonths) + " Months"
+fig.set_title(title, fontweight="bold")
 fig.set_ylabel("Total Distance Run (km)", fontweight="bold")
 fig.set_xlabel("Month", fontweight="bold")
 
+# This part does the heavy lifting, loops through every Nug, all past 12 months, and every activity
 for f, file in enumerate(file_list):
     # Read the input file
     activities = pd.read_csv(file, sep=',', header=0)
@@ -52,9 +54,10 @@ for f, file in enumerate(file_list):
         monthly_totals.append(total_distance)
         total_distance = 0
 
+    # Plot the Nugs efforts for the past X months as a line thing
     plt.plot(months_list, monthly_totals, label=names[f], linewidth=3)
 
-legend_properties = {'weight':'bold'}
+legend_properties = {'weight': 'bold'}
 plt.legend(prop=legend_properties)
 plt.grid('both')
 plt.show()
