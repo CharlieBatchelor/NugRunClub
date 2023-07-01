a451d1824e5d79562d5d321b4697abd8aa0184e3
Charlie Batchelor <charlie@charlies-mbp.home>
2023-07-01 16:02:09 +0100

    Try optimising for mobile view.

--- website/frontend/static/js/views/Dashboard.js
+++ website/frontend/static/js/views/Dashboard.js
@@ -8,11 +8,11 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-            <h1>Nug Run Club 2023!</h1>
+            <h2>Nug Run Club 2023!</h2>
             <p>
                 Watch this space! Here we'll put the current month pie with some graphs of current progress.       
             </p>
-            <img src="images/pie.png" alt="Current Pie" style="width:600px;">
+            <img src="images/pie.png" alt="Current Pie" style="max-width: 100%; height: auto;">
             <p>
                 <a href="/posts" data-link>View previous month winners...</a>.
             </p>
