350a4b70e94f6625900f9f37387cc729f46619c9
Charlie Batchelor <charlie@charlies-mbp.home>
2023-07-02 09:00:53 +0100

    Attempt to make pie larger on mobile.

--- website/frontend/static/js/views/Dashboard.js
+++ website/frontend/static/js/views/Dashboard.js
@@ -12,7 +12,7 @@ export default class extends AbstractView {
             <p>
                 Watch this space! Here we'll put the current month pie with some graphs of current progress.       
             </p>
-            <img src="images/pie.png" alt="Current Pie" style="max-width: 100%; height: auto;">
+            <img src="images/pie.png" alt="Current Pie" style="max-width: 100%; height: auto; width: 80vw; max-height: 50vh;">
             <p>
                 <a href="/posts" data-link>View previous month winners...</a>.
             </p>
