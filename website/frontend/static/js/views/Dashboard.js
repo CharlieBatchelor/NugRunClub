4b4be4f43bf04608205e2499c9bdbeddd66d74d2
Charlie Batchelor <charliebatchelor1@hotmail.co.uk>
2023-08-16 09:06:32 +0100

    Further webpage updates.

--- website/frontend/static/js/views/Dashboard.js
+++ website/frontend/static/js/views/Dashboard.js
@@ -8,12 +8,11 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-            <h1>Nug Run Club 2023!</h1>
+            <h2>Nug Run Club 2023!</h2>
             <p>
                 Watch this space! Here we'll put the current month pie with some graphs of current progress.       
             </p>
-            <img src="images/pie.png" alt="Current Pie" style="max-width: 35%; height: auto;">
-            <img src="images/NRCLogo.jpeg" alt="NRC Logo" style="max-width: 35%; height: auto; ">
+            <img src="images/pie.png" alt="Current Pie" style="max-width: 100%; height: auto;">
             <p>
                 <a href="/lastMonth" data-link>View previous month winners...</a>.
             </p>
